66ac0181e320c9b1026a4559505c07c560b11f31
jinxl@julive.com <jinxl@julive.com>
2021-10-19 22:50:52 +0800

    图的深度优先遍历

--- app/src/main/java/com/julive/textdemo/graph/Bsf_DsfCode.java
+++ app/src/main/java/com/julive/textdemo/graph/Bsf_DsfCode.java
@@ -3,6 +3,7 @@ package com.julive.textdemo.graph;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * Created by jinxianlun
@@ -27,6 +28,32 @@ public class Bsf_DsfCode {
                 if (!set.contains(next)) {
                     set.add(next);
                     queue.add(next);
+                }
+            }
+        }
+    }
+
+    /**
+     * 深度优先遍历
+     */
+    public void dsf(Node start) {
+        if (start == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        HashSet<Node> set = new HashSet<>();
+        stack.add(start);
+        set.add(start);
+        System.out.println(start.value + "");
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println(next.value + "");
+                    break;
                 }
             }
         }
