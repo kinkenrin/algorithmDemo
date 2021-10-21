da0c21e24389a8f17b8b57b4d629635b16144f54
jinxl@julive.com <jinxl@julive.com>
2021-10-21 20:17:00 +0800

    打印字符串全排列

--- app/src/test/java/com/julive/textdemo/PrintAllSubsquences.java
+++ app/src/test/java/com/julive/textdemo/PrintAllSubsquences.java
@@ -14,24 +14,52 @@ public class PrintAllSubsquences {
     @Test
     public void test() {
         String content = "abc";
-        List<String> subs = subs(content);
-        System.out.println(subs.toString());
+//        List<String> subs = subs(content);
+//        System.out.println(subs.toString());
+
+        List<String> strings = permutation2(content);
+        System.out.println(strings.toString());
     }
 
     public List<String> subs(String s) {
         List<String> ans = new ArrayList<>();
-        char[] str = s.toCharArray();
-        String path = "";
-        process(str, 0, ans, path);
+        process(s, 0, ans, "");
         return ans;
     }
 
-    private void process(char[] str, int index, List<String> ans, String path) {
-        if (index == str.length) {
+    private void process(String str, int index, List<String> ans, String path) {
+        if (index == str.length()) {
             ans.add(path);
             return;
         }
+
         process(str, index + 1, ans, path);
-        process(str, index + 1, ans, path + str[index]);
+        process(str, index + 1, ans, path + str.charAt(index));
     }
+
+    public List<String> permutation2(String s) {
+        List<String> ans = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        process1(chars, 0, ans);
+        return ans;
+    }
+
+    private void process1(char[] str, int index, List<String> ans) {
+        if (index == str.length) {
+            ans.add(String.valueOf(str));
+        } else {
+            for (int i = index; i < str.length; i++) {
+                swap(str, index, i);
+                process1(str, index + 1, ans);
+                swap(str, index, i);
+            }
+        }
+    }
+
+    public static void swap(char[] arr, int i, int j) {
+        char a = arr[j];
+        arr[j] = arr[i];
+        arr[i] = a;
+    }
+
 }
